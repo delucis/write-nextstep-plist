6099a8e93d423ed217da7a782775173ff8e13dc4
delucis <swithinbank@gmail.com>
2019-06-16 23:53:04 +0200

    test: Add coverage for bad parameter throws

--- test.js
+++ test.js
@@ -27,3 +27,21 @@ test('write objects', async t => {
   await writePlist(tmp, { foo: {}, bar: { baz: [] } })
   t.is(fs.readFileSync(tmp, 'utf8'), '{ "foo" = { }; "bar" = { "baz" = (  ); }; }\n')
 })
+
+test('throws if filepath is empty (async)', async t => {
+  await t.throwsAsync(async () => writePlist(''))
+})
+
+test('throws if filepath is empty (sync)', t => {
+  t.throws(() => writePlist.sync(''))
+})
+
+test('throws if no data provided (async)', async t => {
+  const tmp = path.join(tempfile(), 'foo')
+  await t.throwsAsync(async () => writePlist(tmp))
+})
+
+test('throws if no data provided (sync)', t => {
+  const tmp = path.join(tempfile(), 'foo')
+  t.throws(() => writePlist.sync(tmp))
+})
